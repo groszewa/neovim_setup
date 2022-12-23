ef0a4aacc939563f444dc2933f15db2e32098ea8
Alexander Groszewski <groszewa@gmail.com>
2022-12-22 20:11:54 -0600

    enable tree-sitter across all envs

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,29 +1,28 @@
-if(not os.getenv('GROSZEWA_VPC')) then
-    require'nvim-treesitter.configs'.setup {
-        -- A list of parser names, or "all"
-        ensure_installed = { "c", "lua", "rust", "bash", "cmake", "cpp", "help", "json", "make", "ninja", "perl", "python", "verilog", "yaml" },
+require'nvim-treesitter.configs'.setup {
+    -- A list of parser names, or "all"
+    ensure_installed = { "c", "lua", "rust", "bash", "cmake", "cpp", "help", "json", "make", "ninja", "perl", "python", "verilog", "yaml" },
 
-        -- Install parsers synchronously (only applied to `ensure_installed`)
-        sync_install = false,
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
 
-        -- Automatically install missing parsers when entering buffer
-        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-        auto_install = true,
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = true,
 
-        ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-        -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+    ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+    -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
-        highlight = {
-            -- `false` will disable the whole extension
-            enable = true,
+    highlight = {
+        -- `false` will disable the whole extension
+        enable = true,
 
-            -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-            -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-            -- the name of the parser)
-            -- list of language that will be disabled
-            -- disable = { "c", "rust" },
-            -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-            -- disable = function(lang, buf)
+        -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+        -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+        -- the name of the parser)
+        -- list of language that will be disabled
+        -- disable = { "c", "rust" },
+        -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+        -- disable = function(lang, buf)
             --     local max_filesize = 100 * 1024 -- 100 KB
             --     local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
             --     if ok and stats and stats.size > max_filesize then
@@ -38,4 +37,3 @@ if(not os.getenv('GROSZEWA_VPC')) then
             additional_vim_regex_highlighting = false,
         },
     }
-end
--- lua/groszewa/packer.lua
+++ lua/groszewa/packer.lua
@@ -18,8 +18,8 @@ return require('packer').startup(function(use)
       -- or                            , branch = '0.1.x',
       requires = { {'nvim-lua/plenary.nvim'} }
   })
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   if(not os.getenv('GROSZEWA_VPC')) then
-      use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
       use('nvim-treesitter/playground')
       use('mbbill/undotree')
       use('tpope/vim-fugitive')
