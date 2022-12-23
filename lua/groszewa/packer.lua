8672c705b8260aa60bdcb12e53b8c1a4c513f806
Alexander Groszewski <groszewa@gmail.com>
2022-12-22 21:26:25 -0600

    remove treesitter from linux since using old nvim (perf)

--- lua/groszewa/packer.lua
+++ lua/groszewa/packer.lua
@@ -18,11 +18,11 @@ return require('packer').startup(function(use)
       -- or                            , branch = '0.1.x',
       requires = { {'nvim-lua/plenary.nvim'} }
   })
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use('nvim-treesitter/playground')
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
   if(not os.getenv('GROSZEWA_VPC')) then
+      use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+      use('nvim-treesitter/playground')
       use {
           'VonHeikemen/lsp-zero.nvim',
           requires = {
